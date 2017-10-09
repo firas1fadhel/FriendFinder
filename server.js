772f08293fe845635f258562fcb9f53f3e6bf003
Firas AlOmairi <firas.lcc@gmail.com>
2017-10-08 23:51:45 -0500

    Setting routes and layouts

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+//Use Heroku port or local 3000
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// Static directory to be served
+app.use(express.static("app/public"));
+//Pulling in routes defined under routing folder
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
