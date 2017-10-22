ac7059c696d1e65513acaf31b892598530dd8373
Firas AlOmairi <firas.lcc@gmail.com>
2017-10-21 23:06:50 -0500

    finished

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 // Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
-//Use Heroku port or local 3000
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3030;
 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-// Static directory to be served
-app.use(express.static("app/public"));
-//Pulling in routes defined under routing folder
-require("./app/routing/htmlRoutes.js")(app);
-require("./app/routing/apiRoutes.js")(app);
 
+// Routes
+// =============================================================
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
+
+
+
+
+
+
+
